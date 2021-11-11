e6b487653fd4fb682c3f6c7c02fbf7d42e9b7af0
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-11 13:35:46 +0530

    Completed the webpack setup.

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,28 @@ module.exports = {
 	resolve: {
 		extensions: ["js", "jsx"],
 	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx/,
+				exclude: "/node_modules",
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-react", "@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.scss/,
+				exclude: "/node_modules",
+				use: ["style-loader", "css-loader", "postcss-loader"], // Note that postcss loader must come before sass-loader
+			},
+		],
+	},
+	devServer: {
+		contentBase: "./src",
+		port: 8080,
+		hot: true,
+	},
 };

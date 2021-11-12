ea54d06c4878c15e9817ee2ecdf3680f3a36160b
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-13 01:56:06 +0530

    Added some extra config

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,7 @@ module.exports = {
         static: {
             directory: path.join(__dirname, "dist"),
         },
+        historyApiFallback: true,
         client: {
             overlay: {
                 warnings: false,

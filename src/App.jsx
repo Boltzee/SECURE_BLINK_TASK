15bcae281ebd89ec9137becb85e610bc4a2e2311
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-13 02:00:23 +0530

    Final changes for app.jsx for today

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import NavBar from "./components/navbar/NavBar";
-import Section1 from "./components/section1/Section1";
-import { useAuthState } from "react-firebase-hooks/auth";
 import db from "./firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./components/homepage/Homepage";
+import Not_found from "./components/not_found/Not_found";
 
 const auth = getAuth();
 
 function App() {
-	let present_user;
-	onAuthStateChanged(auth, (user) => {
-		present_user = user ? user : "";
-	});
-	return (
-		<div className="container-fluid p-0">
-			<NavBar user={present_user} />
-			<Section1 />
-		</div>
-	);
+    let present_user;
+    onAuthStateChanged(auth, (user) => {
+        present_user = user ? user : "";
+    });
+    return (
+        <div className="container-fluid p-0">
+            <Router>
+                <NavBar user={present_user} />
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="*" element={<Not_found />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;

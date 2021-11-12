d70d2092915e5a414e41ffcb8aaec6cbbf1055e5
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-13 02:01:59 +0530

    Created a new file fo the landing page.

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Section1 from "../section1/Section1";
+import { Container } from "react-bootstrap";
+
+function Homepage() {
+    return (
+        <div>
+            <Container>
+                <Section1 />
+            </Container>
+        </div>
+    );
+}
+
+export default Homepage;

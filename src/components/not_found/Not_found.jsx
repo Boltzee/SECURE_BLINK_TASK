66129ec8ffdd0ca7298430ba1198df90c20acb0e
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-14 18:22:52 +0530

    Completed the 404 page

--- src/components/not_found/Not_found.jsx
+++ src/components/not_found/Not_found.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import Not_found_svg from "./Not_found_svg";
+import Not_found_svg from "./Not_found_svg_1.svg";
 import Style from "./Not_found.module.css";
 
 const Not_found = () => {
     return (
         <>
             <Container fluid className={Style.no_user_cont}>
-                <div className={Style.svg_img2}>
-                    <Not_found_svg />
-                </div>
                 <Row className={Style.no_user_row}>
-                    <div className={Style.no_user_div}>
-                        <h4 className="display-5 mb-4">
+                    <div className={`col-sm-4 ${Style.no_user_div}`}>
+                        <h6 className="display-5 mb-4">
                             Page
                             <br /> Not{" "}
-                            <span style={{ color: `coral` }}>Found</span>
-                        </h4>
+                            <span style={{ color: "#01CB63" }}>Found</span>
+                        </h6>
                         <a
                             href="/"
                             type="button"
-                            class="btn btn-dark btn-rounded mb-4"
+                            className="btn btn-dark btn-rounded mb-4"
                         >
                             Back to Home
                         </a>
+                    </div>
+                    <div className={`col-sm-8 ${Style.svg_img2}`}>
+                        <img src={Not_found_svg} width="90%" alt="" />
                     </div>
                 </Row>
             </Container>

2baa43cc45fa31cbcbb283269b77f18170cc8de6
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-14 13:58:42 +0530

    Imported workings diag component

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Section1 from "../section1/Section1";
 import Contact_us from "../contact_us/Contact_us";
 import Reviews from "../Reviews/Reviews";
+import Workings from "../diagram/Workings";
 import { Container } from "react-bootstrap";
 
 function Homepage() {
@@ -9,6 +10,7 @@ function Homepage() {
         <div>
             <Container>
                 <Section1 />
+                <Workings />
                 <Reviews />
             </Container>
             <Contact_us />

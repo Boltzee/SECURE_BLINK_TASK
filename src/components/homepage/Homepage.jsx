de2346269082aac7949d1d5ea93436eabbaa31b1
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-14 13:01:52 +0530

    Imported Reviews section

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Section1 from "../section1/Section1";
 import Contact_us from "../contact_us/Contact_us";
+import Reviews from "../Reviews/Reviews";
 import { Container } from "react-bootstrap";
 
 function Homepage() {
@@ -8,6 +9,7 @@ function Homepage() {
         <div>
             <Container>
                 <Section1 />
+                <Reviews />
             </Container>
             <Contact_us />
         </div>

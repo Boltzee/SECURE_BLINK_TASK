f0fe302c54f611b6919fc5ba9ece34e2c836b621
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-11-14 06:29:06 +0530

    Final Navbar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Style from "./Navbar.module.css";
-import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    GoogleAuthProvider,
-} from "firebase/auth";
-import db, { provider } from "../../firebase";
-import Google_svg from "./Google_svg";
 import "./Navbar.css";
 import brand from "./Brand_icon.svg";
 
-import {
-    Container,
-    Row,
-    Col,
-    Navbar,
-    Nav,
-    Modal,
-    Form,
-    Button,
-} from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 
-const auth = getAuth();
 function NavBar({ user }) {
     console.log("The user present is ", user);
 
@@ -32,37 +13,29 @@ function NavBar({ user }) {
             <Navbar
                 id="main_navbar"
                 sticky="top"
-                expand="lg"
-                bg="light"
                 className={`${Style.main_navbar}`}
             >
                 <Navbar.Brand href="/">
                     <img src={brand} className="_logo" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav
-                        className="n"
-                        style={{ alignItems: `center`, width: `100%` }}
-                        id="nav-cont"
-                    >
-                        <Nav.Link
-                            href="#"
-                            className={`${Style.n2} ms-auto`}
-                            id="left"
-                        >
-                            Login
-                        </Nav.Link>
+                <Nav
+                    className="n"
+                    style={{
+                        alignItems: `center`,
+                        width: `100%`,
+                        paddingRight: `4.5rem`,
+                    }}
+                    id="nav-cont"
+                >
+                    <Nav.Link href="#" className={`${Style.n2}`} id="left">
+                        Login
+                    </Nav.Link>
 
-                        <Nav.Link
-                            href="#"
-                            className={`${Style.n2} ms-auto`}
-                            id="left"
-                        >
-                            <i>Register</i>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                    <Nav.Link href="#" className={`${Style.n2}`} id="right">
+                        <i>Register</i>
+                    </Nav.Link>
+                </Nav>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
             </Navbar>
         </>
     );
